bb5c43de717b37feda3ec3c6dda675f4ff8c138f
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-07-02 12:18:05 -0600

    Random password is now generating

--- script.js
+++ script.js
@@ -10,46 +10,56 @@ function generatePassword() {
   var password = "";
   var passwordChar = "";
 // Creates prompt to select password length
-  var charLength = prompt("How many characters would you like your password to contain? Password must be between 8-128.")
+  var charLength = window.prompt("How many characters would you like your password to contain? Password must be between 8-128.")
   charLength = parseInt(charLength);
 
   if (charLength < 8) {
-    alert("Password length must be at least 8 characters.");
+    window.alert("Password length must be at least 8 characters.");
     return "";
   }
 
   if (charLength > 128) {
-    alert("Password length must not exceed more than 128 characters.");
+    window.alert("Password length must not exceed more than 128 characters.");
     return "";
   }
 
 // Creates confirmation for lowercase character
-  var lowercaseSelect = confirm("Click OK to confirm using lowercase characters.");
+  var lowercaseSelect = window.confirm("Click OK to confirm using lowercase characters.");
 
   if (lowercaseSelect) {
-    passwordChar += lowercaseSelect;
+    passwordChar += charLowercase;
   }
 
 // Creates confirmation for uppercase character
-  var uppercaseSelect = confirm("Click OK to confirm using uppercase characters.");
+  var uppercaseSelect = window.confirm("Click OK to confirm using uppercase characters.");
 
   if (uppercaseSelect) {
-    passwordChar += uppercaseSelect;
+    passwordChar += charUppercase;
   }
 
 // Creates confirmation for numerical character
-  var numericalSelect = confirm("Click OK to confirm using numerical character.");
+  var numericalSelect = window.confirm("Click OK to confirm using numerical character.");
 
   if (numericalSelect) {
-    passwordChar += numericalSelect;
+    passwordChar += charNumerical;
   }
 
 // Creates confirmation for special character
-  var specialSelect = confirm("Click OK to confirm using special characters.");
+  var specialSelect = window.confirm("Click OK to confirm using special characters.");
 
   if (specialSelect) {
-    passwordChar += specialSelect;
+    passwordChar += charSpecial;
   }
+
+  if (!passwordChar) {
+    alert("Must select at least one character type.");
+    return "";
+  }
+
+  for (var index = 0; index < charLength; index++) {
+    password += passwordChar[Math.floor(Math.random() * passwordChar.length)]
+  }
+  return password;
 }
 
 // Write password to the #password input
