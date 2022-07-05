8efaf236ad4be4a8cccdf0dd06e0de80eec15444
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-07-04 18:31:53 -0600

    Finished comments for javascript

--- script.js
+++ script.js
@@ -1,58 +1,64 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Defines varibles as a string
 var charLowercase = "abcdefghijklmnopqrstuvwxyz";
 var charUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var charNumerical = "0123456789";
 var charSpecial = "`~!@#$%^&*()-_=+[{]}\|;:'<.>/?"
 
+// Function used to generate password
 function generatePassword() {
   var password = "";
   var passwordChar = "";
-// Creates prompt to select password length
+
+// Creates prompt for user to select password length
   var charLength = window.prompt("How many characters would you like your password to contain? Password must be between 8-128.")
   charLength = parseInt(charLength);
 
+// If chosen character length is less than 8 an alert is returned to user
   if (charLength < 8) {
     window.alert("Password length must be at least 8 characters.");
     return "";
   }
 
+// If chosen character length is greater than 128 an alert is returned to user
   if (charLength > 128) {
     window.alert("Password length must not exceed more than 128 characters.");
     return "";
   }
 
-// Creates confirmation for lowercase character
+// Creates prompt for user to confirm if lowercase character will be used in password.
   var lowercaseSelect = window.confirm("Click OK to confirm using lowercase characters.");
 
   if (lowercaseSelect) {
     passwordChar += charLowercase;
   }
 
-// Creates confirmation for uppercase character
+// Creates prompt for user to confirm if uppercase character will be used in password.
   var uppercaseSelect = window.confirm("Click OK to confirm using uppercase characters.");
 
   if (uppercaseSelect) {
     passwordChar += charUppercase;
   }
 
-// Creates confirmation for numerical character
+// Creates prompt for user to confirm if numerical character will be used in password.
   var numericalSelect = window.confirm("Click OK to confirm using numerical character.");
 
   if (numericalSelect) {
     passwordChar += charNumerical;
   }
 
-// Creates confirmation for special character
+// Creates prompt for user to confirm if special character will be used in password.
   var specialSelect = window.confirm("Click OK to confirm using special characters.");
 
   if (specialSelect) {
     passwordChar += charSpecial;
   }
 
+// Creates prompt if user did not select at least one character type.
   if (!passwordChar) {
-    alert("Must select at least one character type.");
+    window.alert("Must select at least one character type.");
     return "";
   }
 
